5495aec8591957a45786922ec1e2f1c5ce686535
Ivan Sentemon <124244721+sentemon@users.noreply.github.com>
2023-09-21 21:37:59 +0200

    zadanie 1

--- KP2.cs
+++ KP2.cs
@@ -3,6 +3,16 @@ using System;
 public class Program {
 
   public static void Main(string[] args) {
-    Console.WriteLine("hello world");
+    // zadanie 1
+    Console.WriteLine("Wprowadź liczbę:");
+    int a1 = int.Parse(Console.ReadLine());
+    if (a1 % 3 == 0)
+    {
+      Console.WriteLine("Liczba " + a1 + " jest podzilna przez 3");
+    }
+    else
+    {
+     Console.WriteLine("Liczba " + a1 + " nie jest podzilna przez 3"); 
+    }
   }
 }
