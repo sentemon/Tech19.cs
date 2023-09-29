5c266a5b7cac4cbda697957c47d579f86ad74477
Ivan Sentemon <124244721+sentemon@users.noreply.github.com>
2023-09-29 16:53:05 +0200

    Update KP2a.cs

--- KP2a.cs
+++ KP2a.cs
@@ -3,6 +3,18 @@ using System;
 public class Program {
 
   public static void Main(string[] args) {
-    
+    // zadanie 1
+    Console.WriteLine("Wprowadź dwie liczby: ");
+    int a1 = int.Parse(Console.ReadLine());
+    int b1 = int.Parse(Console.ReadLine());
+
+    if ((a1 + b1) % 2 == 0)
+    {
+      Console.WriteLine("TAK");
+    }
+    else
+    {
+      Console.WriteLine("NIE");
+    }
   }
 }
