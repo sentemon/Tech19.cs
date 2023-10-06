9432824f4f4e8536f3b718b9263ea941675b535f
Ivan Sentemon <124244721+sentemon@users.noreply.github.com>
2023-10-06 21:50:31 +0200

    zad2 & update

--- KartyPracy/KP3.cs
+++ KartyPracy/KP3.cs
@@ -4,13 +4,18 @@ public class Program {
 
     public static void Main(string[] args) {
         // zadanie 1
-        Console.WriteLine("Wpisz liczbę n:");
-
-        int n = int.Parse(Console.ReadLine());
-
-        for (int i = 0; i < n; i++) 
-        {
+        for (int i = 0; i < n; i++)
+		{
             Console.WriteLine(Math.Pow(i, 3) + 3);
+        }
+
+        // zadanie 2
+        for (int i = 100; i < 1000; i++)
+		{
+            if (i % 15 == 0)
+			{
+                Console.WriteLine(i);
+            }
         }
     }
 }
