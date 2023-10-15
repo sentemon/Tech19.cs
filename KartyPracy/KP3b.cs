d4ba42ba1743578cc8f8e09288432a662ccfe4c1
Ivan <sentemon.i@gmail.com>
2023-10-15 17:07:38 +0200

    zad1

--- KartyPracy/KP3b.cs
+++ KartyPracy/KP3b.cs
@@ -1,9 +1,12 @@
-// test
 using System; 
 
 public class Program {
 
   public static void Main(string[] args) {
-    Console.WriteLine("lk");
+    // Zadanie 1
+    for (int i = 1; i <= 30; i++)
+    {
+      Console.Write(i + " ");
+    }
   }
 }
