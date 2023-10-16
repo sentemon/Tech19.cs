ccc8341874c8e4507178a89937f486d890870057
Ivan Sentemon <124244721+sentemon@users.noreply.github.com>
2023-10-16 18:57:01 +0200

    Update KP3b.cs

--- KartyPracy/KP3b.cs
+++ KartyPracy/KP3b.cs
@@ -8,6 +8,7 @@ public class Program {
     {
       Console.Write(i + " ");
     }
+    
     // Zadanie 2
     int n = int.Parse(Console.ReadLine());
     for (int i = 1; i <= n; i++)
@@ -21,10 +22,20 @@ public class Program {
         n++;
       }
     }
+    
     // Zadanie 3
     for (int i = 1137; i < 10000; i+=379)
     {
       Console.Write(i + " ");
+    }
+    
+    // Zadanie 4
+    for (int i = 100; i < 1000; i++)
+    {
+      if (i % 5 == 0 || i % 6 == 0 || i % 11 == 0) // nie jestem pewien czy dobrze zrozumiałem polecenie
+      {
+        Console.Write(i + " ");
+      } 
     }
   }
 }
