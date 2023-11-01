32ee85f143d8c4df9b06bf1d9a33b12238afe8d9
Ivan Sentemon <124244721+sentemon@users.noreply.github.com>
2023-11-01 07:52:07 +0100

    zad2

--- KartyPracy/KP3a.cs
+++ KartyPracy/KP3a.cs
@@ -11,5 +11,21 @@ public class Program {
     {
       Console.Write("∗ − | ");
     }
+    
+    // Zadanie 2
+    Console.Write("Wprowadź liczbę n: ");
+    int n = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write("∗");
+        }
+        if (i < n)
+        {
+            Console.Write("||");
+        }
+    }
   }
 }
