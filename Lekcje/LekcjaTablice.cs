29306926eebab9e398e25f6bd97c97d6a2c073af
Ivan Sentemon <sentemon.i@gmail.com>
2023-12-03 17:59:15 +0100

    Add tasks to generate random numbers and count even and odd numbers in an array

--- Lekcje/LekcjaTablice.cs
+++ Lekcje/LekcjaTablice.cs
@@ -6,6 +6,7 @@ namespace Lessons
     {
         public static void Main(string[] args)
         {
+            // zadanie 1
             int[] RandomNumbers = new int[10];
             Random r = new Random();
 
@@ -16,6 +17,46 @@ namespace Lessons
             foreach (int j in RandomNumbers)
             {
                 Console.Write(j + " ");
+            }
+
+            // zadanie 2
+            int[] RandomNumbers = new int[10];
+            Random r = new Random();
+
+            int CzyParzysta = 0;
+
+            for (int i = 0; i < RandomNumbers.Length; i++)
+            {
+                RandomNumbers[i] = r.Next(0, 100);
+            }
+            
+            for (int i = 0; i < RandomNumbers.Length; i++)
+            {
+                Console.Write(RandomNumbers[i] + " ");
+            }
+
+            for (int i = 0; i < RandomNumbers.Length; i++)
+            {
+                if (RandomNumbers[i] % 2 == 0)
+                {
+                    CzyParzysta++;
+                }
+                else
+                {
+                    CzyParzysta--;
+                }
+            }
+            if (CzyParzysta > 0)
+            {
+                Console.WriteLine("\nW tablicy jest więcej liczb parzystych");
+            }
+            else if (CzyParzysta < 0)
+            {
+                Console.WriteLine("\nW tablicy jest więcej liczb nieparzystych");
+            }
+            else
+            {
+                Console.WriteLine("\nW tablicy jest tyle samo liczb parzystych i nieparzystych");
             }
         }
     }
