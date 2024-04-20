4ef1f535256b0df57201043e035788d6a797d737
Ivan Sentemon <sentemon.i@gmail.com>
2024-04-20 10:55:20 +0200

    Skończona KP nr 3a

--- KartyPracy/KP3a.cs
+++ KartyPracy/KP3a.cs
@@ -51,19 +51,64 @@ public class Program {
     // Zadanie 4
     Console.Write("Wprowadź liczbę n: ");
     int n = int.Parse(Console.ReadLine());
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ((i + j) == (n - 1) / 2 || Math.Abs(i - j) == (n - 1) / 2 || (i + j) == (n - 1) + (n - 1) / 2)
+                Console.Write("* ");
+            else
+                Console.Write("  ");
+        }
+
+        Console.WriteLine();
+    }
 
     // Zadanie 5
     Console.Write("Wprowadź liczbę n: ");
     int n = int.Parse(Console.ReadLine());
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j == n / 2)      Console.Write("* ");
+            else if (i == n / 2) Console.Write("— ");
+            else                 Console.Write("  ");
+        }
+
+        Console.WriteLine();
+    }
 
     // Zadanie 6
     Console.Write("Wprowadź liczbę n: ");
     int n = int.Parse(Console.ReadLine());
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i + j == n - 1) Console.Write("? ");
+            else if (i == j)    Console.Write("* ");
+            else                Console.Write("  ");
+        }
 
+        Console.WriteLine();
+    }
 
     // Zadanie 7
     Console.Write("Wprowadź liczbę n: ");
     int n = int.Parse(Console.ReadLine());
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1 || (i == j && i == n / 2 && j == n / 2)) 
+                Console.Write("* ");
+            else                
+                Console.Write("  ");
+        }
+
+        Console.WriteLine();
+    }
 
   }
 }
